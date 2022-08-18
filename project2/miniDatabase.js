3f77749551ffe483beb13d4d210358b5fdcdfc0f
EnebeliEmmanuel <iemmaenebeli@gmail.com>
2022-08-18 21:35:32 +0100

    correct

--- project2/miniDatabase.js
+++ project2/miniDatabase.js
@@ -8,7 +8,7 @@ const account0 = new ethers.Wallet("0xac0974bec39a17e36ba4a6b4d238ff944bacb478cb
 const account1 = new ethers.Wallet("0x59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d", provider);
 const account19 = new ethers.Wallet("0xdf57089febbacf7ba0bc227dafbffa9fc08a93fdc68e1e42411a14efcf23656e", provider);
 
-const CONTRACT_ADDRESS = "0x65b4744E5eA68A92B04507C82F52b9028fcfb191"; // address of the new contract.
+const CONTRACT_ADDRESS = "0x5FbDB2315678afecb367f032d93F642f64180aa3"; // address of the new contract.
 const { abi } = require("./artifacts/contracts/miniDatabase.sol/miniDatabase.json");
 
 const balances = async () => {
@@ -31,6 +31,7 @@ const sendTx = async () => {
 
 const contractInteraction = async () => {
     const contract = new ethers.Contract(CONTRACT_ADDRESS, abi, provider);
-    const result = await contract.deposit();
+    const result = await contract.hello();
     console.log(`result: ${result}`);
 }
+
